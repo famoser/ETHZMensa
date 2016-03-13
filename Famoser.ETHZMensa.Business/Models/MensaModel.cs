9097febf974eae17785a785d3655ea596abe232e
Florian Moser <git@famoser.ch>
2016-03-13 23:14:05 +0100

    some models

---
+++ Famoser.ETHZMensa.Business/Models/MensaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Famoser.ETHZMensa.Business.Models
+{
+    class MensaModel
+    {
+        public string OpeningTimes { get; set; }
+        public ObservableCollection<MenuModel> MenuMittags { get; set; }
+        public ObservableCollection<MenuModel> MenuAbends { get; set; }
+    }
+}
---
+++ Famoser.ETHZMensa.Business/Models/MenuModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Famoser.ETHZMensa.Business.Models
+{
+    public class MenuModel
+    {
+        public string Title { get; set; }
+        public string[] Prices { get; set; }
+        public string Description { get; set; }
+    }
+}
